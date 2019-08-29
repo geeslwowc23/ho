f4c1ad7c09000fd025042fac5dc9ae3362d900fb
Camilo Meli <cmeli@localhost.localdomain>
2019-08-29 18:50:46 -0400

    poniendo datos en la rama de QA

--- sql/modelo.sql
+++ sql/modelo.sql
@@ -15,7 +15,7 @@ create table if not exists persona (
 create table if not exists usuario (
   id serial primary key,
   usuario varchar(255) not null,
-  contraseña varchar(255) not null,
+  contrasena varchar(255) not null,
   persona_id bigint(20) unsigned not null,
   foreign key(persona_id) references persona(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
